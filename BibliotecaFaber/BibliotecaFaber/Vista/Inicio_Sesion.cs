355224a5f8b9287fb369f52a93dfbe05475080c0
Víctor Vásquez <sonry001@gmail.com>
2017-05-16 00:34:00 -0400

    Login Form

---
+++ BibliotecaFaber/BibliotecaFaber/Vista/Inicio_Sesion.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BibliotecaFaber.Vista {
+    public partial class Inicio_Sesion : Form {
+        public Inicio_Sesion() {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e) {
+
+        }
+    }
+}
