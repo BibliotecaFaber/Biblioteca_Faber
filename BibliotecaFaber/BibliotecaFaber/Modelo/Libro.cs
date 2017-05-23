4b2a3651d267e8799a01f18e4c804714a9e367a9
Víctor Vásquez <sonry001@gmail.com>
2017-05-22 22:02:54 -0400

    Cambios de Bloosky

---
+++ BibliotecaFaber/BibliotecaFaber/Modelo/Libro.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BibliotecaFaber.Modelo {
+    class Libro {
+
+        private int id;
+        private string nombre;
+        private string autor;
+        private string editorial;
+        private int fila;
+        private int columna;
+        private int ID_estante;
+        private bool en_prestamo;
+
+        public Libro(int id, string nombre, string autor, string editorial, int fila, int columna, int id_estante) {
+            this.Id = id;
+            this.Nombre = nombre;
+            this.Autor = autor;
+            this.Editorial = editorial;
+            this.Fila = fila;
+            this.Columna = columna;
+            this.ID_estante = id_estante;
+
+            this.En_prestamo = false;
+        }
+
+        public int Id {
+            get => id;
+            set => id = value;
+        }
+        public string Nombre {
+            get => nombre;
+            set => nombre = value;
+        }
+        public string Autor {
+            get => autor;
+            set => autor = value;
+        }
+        public string Editorial {
+            get => editorial;
+            set => editorial = value;
+        }
+        public int Fila {
+            get => fila;
+            set => fila = value;
+        }
+        public int Columna {
+            get => columna;
+            set => columna = value;
+        }
+        public int id_estante {
+            get => ID_estante;
+            set => ID_estante = value;
+        }
+        public bool En_prestamo {
+            get => en_prestamo;
+            set => en_prestamo = value;
+        }
+    }
+}
---
+++ BibliotecaFaber/BibliotecaFaber/Modelo/Prestamo.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BibliotecaFaber.Modelo {
+    class Prestamo {
+        private int ID_prestamo;
+        private bool estado_prestamo;
+        private Cliente cliente;
+        private Libro libro;
+        private Libro libro2;
+        private Libro libro3;
+
+        public Prestamo(int iD_prestamo, bool estado_prestamo, Cliente cliente, Libro libro, Libro libro2, Libro libro3) {
+            ID_prestamo1 = iD_prestamo;
+            this.Estado_prestamo = estado_prestamo;
+            this.Cliente = cliente;
+            this.Libro = libro;
+            this.Libro2 = libro2;
+            this.Libro3 = libro3;
+        }
+
+        public int ID_prestamo1 {
+            get => ID_prestamo;
+            set => ID_prestamo = value;
+        }
+        public bool Estado_prestamo {
+            get => estado_prestamo;
+            set => estado_prestamo = value;
+        }
+        internal Cliente Cliente {
+            get => cliente;
+            set => cliente = value;
+        }
+        internal Libro Libro {
+            get => libro;
+            set => libro = value;
+        }
+        internal Libro Libro2 {
+            get => libro2;
+            set => libro2 = value;
+        }
+        internal Libro Libro3 {
+            get => libro3;
+            set => libro3 = value;
+        }
+    }
+}
